5afe77bc6e74da341164e85df8eeb0b451b6231e
KINTAUDI NDINDA BENJAMINE <benjaminendinda012@gmail.com>
2025-06-27 21:25:44 +0100

    Add files via upload

--- ecole.js
+++ ecole.js
@@ -36,3 +36,11 @@
             slidesTrack.style.animation = '';
         });
     });
+
+    const menuIcon = document.querySelector("#menu-burger");
+        const nav = document.querySelector("nav");
+
+        menuIcon.addEventListener("click", () => {
+            menuIcon.classList.toggle("bx-x");
+            nav.classList.toggle("active");
+        });
--- etablissement.js
+++ etablissement.js
@@ -7,3 +7,11 @@
                 track.scrollBy({ left: 250, behavior: 'smooth' });
             };
         })();
+
+          const menuIcon = document.querySelector("#menu-burger");
+        const nav = document.querySelector("nav");
+
+        menuIcon.addEventListener("click", () => {
+            menuIcon.classList.toggle("bx-x");
+            nav.classList.toggle("active");
+        });
